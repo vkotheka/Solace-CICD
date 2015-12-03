b7a8ff9262f8c4bb022e107e958b43c2206f5644
Vidyadhar Kothekar <vidyadhar@Vidyadhars-MacBook-Pro.local>
2015-12-03 21:56:21 +1100

    VPN Creation via CD Pipeline

---
+++ provision.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+IP=52.91.167.19:8080
+CRED=admin:admin
+
+CREATE_VPN="\
+<rpc semp-version=\"soltr/7_1_1\">
+    <create>
+        <message-vpn>
+            <vpn-name>Solace-CICD</vpn-name>
+        </message-vpn>
+    </create>
+</rpc>\
+"
+
+START_VPN="\
+<rpc semp-version=\"soltr/7_1_1\">
+    <message-vpn>
+        <vpn-name>Solace-CICD</vpn-name>
+        <no>
+            <shutdown></shutdown>
+        </no>
+    </message-vpn>
+</rpc>
+\"
+echo "${CREATE_VPN}" | curl -d @- -u "${CRED}" http://${IP}/SEMP
+echo "${START_VPN}" | curl -d @- -u "${CRED}" http://${IP}/SEMP
+
+
+
