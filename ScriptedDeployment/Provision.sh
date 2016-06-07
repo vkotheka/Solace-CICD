c900cd3e51aa212951aa392984244417818a3829
Vidyadhar Kothekar <vidyadhar@Vidyadhars-MacBook-Pro.local>
2016-06-07 19:55:30 +1000

    Modified Provision.sh with correct SEMP XML shell file paths

--- ScriptedDeployment/Provision.sh
+++ ScriptedDeployment/Provision.sh
@@ -3,10 +3,10 @@
 
 for ip in $(cat ./ScriptedDeployment/PubHosts.txt)
 do
-    ./mqtt-poc-pub-semp-xml.sh $1 $ip
+    ./ScriptedDeployment/mqtt-poc-pub-semp-xml.sh $1 $ip
 done
 
 for ip in $(cat ./ScriptedDeployment/SubHosts.txt)
 do
-    ./mqtt-poc-sub-semp-xml.sh $1 $ip
+    ./ScriptedDeployment/mqtt-poc-sub-semp-xml.sh $1 $ip
 done
