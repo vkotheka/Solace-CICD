cd9ae8b63dfbb9260f5841c733b9933475ac3315
Vidyadhar Kothekar <vidyadhar@Vidyadhars-MacBook-Pro.local>
2016-06-07 19:51:22 +1000

    Modified Provision.sh with Pubhosts and Subhosts locations

--- ScriptedDeployment/Provision.sh
+++ ScriptedDeployment/Provision.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-for ip in $(cat PubHosts.txt)
+for ip in $(cat ./PubHosts.txt)
 do
     ./mqtt-poc-pub-semp-xml.sh $1 $ip
 done
 
-for ip in $(cat SubHosts.txt)
+for ip in $(cat ./SubHosts.txt)
 do
     ./mqtt-poc-sub-semp-xml.sh $1 $ip
 done
